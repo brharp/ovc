274cda4047392f4a0f72788bf5522736056f20fc
M. Brent Harp <brharp@uoguelph.ca>
2021-03-18 22:21:55 -0400

    Add styled components plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sass`,
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-google-analytics`,
