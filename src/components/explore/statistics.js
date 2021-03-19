b902960573231f39af376e60b8981f7d00ff7a9a
M. Brent Harp <brharp@uoguelph.ca>
2021-03-19 12:28:48 -0400

    Remove side padding from explore/statistics

--- src/components/explore/statistics.js
+++ src/components/explore/statistics.js
@@ -25,9 +25,10 @@ const Mask = styled.div`
 const Content = styled.div`
   display: grid;
   grid-area: 1/1;
-  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
   grid-gap: 32px;
-  padding: 32px;
+  padding-top: 32px;
+  padding-bottom: 32px;
   justify-items: center;
   align-items: center;
   justify-content: space-around;
