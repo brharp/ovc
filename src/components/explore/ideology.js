df76beb1dea95f1cc57c85f71f7a33f03f9baede
M. Brent Harp <brharp@uoguelph.ca>
2021-03-19 14:54:41 -0400

    Fix layout of ideology on mobile

--- src/components/explore/ideology.js
+++ src/components/explore/ideology.js
@@ -1,13 +1,13 @@
-import React from "react";
-import { withPrefix } from "gatsby";
-import styled from "styled-components";
-import { Container, Row, Col } from "react-bootstrap";
-import Dinkus from "../shared/dinkus";
-import content from "../../../content/explore/ideology.yml";
+import React from "react"
+import { withPrefix } from "gatsby"
+import styled from "styled-components"
+import { Container } from "react-bootstrap"
+import Dinkus from "../shared/dinkus"
+import content from "../../../content/explore/ideology.yml"
 
 const Section = styled.div`
-  padding-top: 8rem;
-  padding-bottom: 8rem;
+  padding-top: 32px;
+  padding-bottom: 32px;;
   text-align: center;
   color: var(--light);
   background-blend-mode: overlay;
@@ -16,22 +16,32 @@ const Section = styled.div`
   & h2 { color: var(--light); }
 `
 
+const Content = styled.div`
+  display: grid;
+  grid-template-rows: auto;
+  grid-gap: 32px;
+  justify-items: evenly;
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+`
+
 class Ideology extends React.Component {
   render() {
     return (
       <Section>
         <Container>
-          <Row>
-            {
-              content.statements.map((data, index) => (
-                <Col key={index}>
-                  <h2>{data.title}</h2>
-                  <Dinkus/>
-                  <p>{data.text}</p>
-                </Col>
-              ))
-            }
-          </Row>
+          <Content>
+          {
+            content.statements.map((data, index) => (
+              <div key={index}>
+                <h2>{data.title}</h2>
+                <Dinkus/>
+                <p>{data.text}</p>
+              </div>
+            ))
+          }
+          </Content>
         </Container>
       </Section>
     )
