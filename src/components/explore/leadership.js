176c824210189f969ee00e1b0a401e3b3bc4a7f6
M. Brent Harp <brharp@uoguelph.ca>
2021-03-22 16:02:51 -0400

    Add crazy leadership dropdown voodoo

--- src/components/explore/leadership.js
+++ src/components/explore/leadership.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import { Container, Card, Popover, OverlayTrigger } from "react-bootstrap";
+import { Accordion, Button, Container, Card, CardGroup  } from "react-bootstrap";
+import { FaAngleDown } from "react-icons/fa"
 import content from "../../../content/explore/leadership.yml";
 
 const Section = styled.div`
@@ -8,71 +9,46 @@ const Section = styled.div`
   padding-bottom: 24px;
 `
 
-const Leaders = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-gap: 12px;
-`
-
-const LeaderCard = styled(Card)`
-  border-left: 4px;
-  border-left-color: var(--red);
-  border-left-style: solid;
-  cursor: pointer;
-`
-
-const Leader = (props) => {
-  const popover = (
-    <Popover id="popover-leader"
-        style={{background: "#F7F8FB", maxWidth:"800px"}}
-      >
-      <Popover.Content>
-        <h3>
-          <span className="text-dark">
-            {props.name}
-          </span>
-          &nbsp; {props.title}
-        </h3>
-        <p>{props.bio}</p>
-      </Popover.Content>
-    </Popover>
-  )
-  return (
-    <OverlayTrigger
-        trigger="click"
-        placement="bottom"
-        overlay={popover}
-        rootClose="true"
-      >
-      <LeaderCard>
-        <Card.Body>
-          <Card.Title>{props.unit}</Card.Title>
-          <Card.Subtitle>{props.title}</Card.Subtitle>
-          <Card.Text>{props.name}</Card.Text>
-        </Card.Body>
-      </LeaderCard>
-    </OverlayTrigger>
-  )
-}
-
+                      //gridArea: `${(index+1)/3} / ${2*(index+1)-1}`
 class Leadership extends React.Component {
   render() {
     return (
       <Section>
         <Container>
           <h2>{content.heading}</h2>
-          <Leaders>
-            {
+          <Accordion style={{
+              display: "grid",
+              gridTemplateColumns: "1fr 1fr 1fr",
+              gridGap: "16px",
+            }}>
+              {
               content.leaders.map((data,index) => (
-                <Leader key={index}
-                        unit={data.unit}
-                        title={data.title}
-                        name={data.name}
-                        bio={data.bio}
-                />
+              <>
+                <div key={index} style={{
+                      gridArea: `${Math.floor(index/3)*2+1} / ${index%3+1}`,
+                      padding: "1px"
+                    }}>
+                  <Card style={{ borderLeft: "4px solid var(--red)" }}>
+                    <Card.Body>
+                      <h4 className="text-dark">{data.unit}</h4>
+                      <Card.Text>
+                        <h6>{data.title}</h6>
+                        <h6>{data.name}</h6> 
+                      </Card.Text>
+                    </Card.Body>
+                <Accordion.Toggle as={Button} variant="link" eventKey={`${index}`}>
+                  <FaAngleDown />
+                </Accordion.Toggle>
+                </Card>
+                </div>
+                <Accordion.Collapse key={index} eventKey={`${index}`}
+                  style={{ gridArea: `${Math.floor(index/3)*2+2} / 1 / ${Math.floor(index/3)*2+2} / 4` }}>
+                  <div>{ data.bio }</div>
+                </Accordion.Collapse>
+                </>
               ))
-            }
-          </Leaders>
+              }
+          </Accordion>
         </Container>
       </Section>
     )
