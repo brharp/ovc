1f413d37e2a781164e1ea7bb96210e26b01a3199
M. Brent Harp <brharp@uoguelph.ca>
2021-03-24 10:22:02 -0400

    Remove McDonald's color scheme from home page more info section

--- src/components/home/moreinfo.js
+++ src/components/home/moreinfo.js
@@ -3,72 +3,69 @@ import { StaticImage } from 'gatsby-plugin-image';
 import styled from "styled-components";
 import MoreInfoData from "../../../content/home/moreinfo.yml"
 
-const MoreInfoWrapper = styled.div`
-  position: relative;
-  z-index: 1;
-  overflow: hidden;
+const Section = styled.div`
+  display: grid;
   max-width: 1920px;
   margin-left: auto;
   margin-right: auto;
-`
-
-const CoverImage = styled.div`
-  > .gatsby-image-wrapper {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    z-index: -1;
-  }
-`
-
-const MoreInfoGrid = styled.div`
   @media (min-width: 992px) {
-    display: grid;
-    grid-template-columns: 3fr 5fr 3fr;
-    grid-template-rows: repeat(2, 1fr);
-    opacity: 0.9;
+    grid-template-columns: repeat(3, 1fr);
   }
+`
+
+const Box = styled.div`
+  display: grid;
+`
+
+const Mask = styled.div`
+  grid-area: 1/1;
+  background: rgba(0, 0, 0, 0.6);
+  position: relative;
+`
+
+const Content = styled.div`
+  grid-area: 1/1;
+  position: relative;
+  padding: 24px;
+  align-self: end;
+  color: var(--light);
+  & h2 { 
+    color: var(--light);
+    text-transform: uppercase;
+    font-size: 3.5rem;
+    border-bottom: 1px solid var(--light);
+    padding-bottom: 1rem;
+  }
+  & h3 { color: var(--yellow); }
 `
 
 function MoreInfo(props) {
-    const zebra = props.index % 2;
     return (
-	    <div key={`feature_${props.index}`} style={{
-		backgroundColor: zebra ? "var(--red)" : "var(--yellow)",
-		padding: "32px 32px 100px",
-		position: "relative"
-	    }}>
-	    <h2 style={{
-		color: zebra ? "white" : "#333",
-		textTransform: "uppercase"
-	    }}>{props.title}</h2>
-	    <h3 style={{
-		color: zebra ? "var(--yellow)" : "var(--red)",
-	    }}>{props.subtitle}</h3>
-	    <p style={{
-		color: zebra ? "white" : "var(--dark)"
-	    }}>{props.description}</p>
-	    <a href="https://ovc.uoguelph.ca/" className={`btn btn-outline-${zebra?'light':'dark'}`}
-	        style={{ position: "absolute", bottom: "32px", textTransform: "uppercase"
-	    }}>See More</a>
-	    </div>
+      <Box>
+        <StaticImage src="../../images/golden-retriever.jpg" alt=""
+                     layout="fullWidth" style={{gridArea: "1/1"}} />
+        <Mask />
+	<Content>
+          <h2>{props.title}</h2>
+	  <h3>{props.subtitle}</h3>
+	  <p>{props.description}</p>
+	  <a href="https://ovc.uoguelph.ca/" className='btn btn-lg btn-outline-light'>
+	    See More
+	  </a>
+	</Content>
+      </Box>
     )
 }
 
 const MoreInfoComponent = ( props ) =>
-  <MoreInfoWrapper>
-    <CoverImage>
-      <StaticImage src="../images/people.jpg" alt="" layout="fullWidth"/>
-    </CoverImage>
-    <MoreInfoGrid>
-      {
-        MoreInfoData.map((data,index) => {
-          return <MoreInfo key={`feature_${index}`} index={index} title={data.title} subtitle={data.subtitle}
-                          description={data.description} />
-        })
-      }
-    </MoreInfoGrid>
-  </MoreInfoWrapper>
+  <Section>
+    {
+      MoreInfoData.map((data,index) => {
+        return <MoreInfo key={`feature_${index}`} index={index} title={data.title} subtitle={data.subtitle}
+                        description={data.description} />
+    })
+    }
+  </Section>
 
 export default MoreInfoComponent
 
