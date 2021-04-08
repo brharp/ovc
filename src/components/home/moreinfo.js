19832063d83e5328ba1f4bc472aebf8f45adb94d
M. Brent Harp <brharp@uoguelph.ca>
2021-04-08 11:43:04 -0400

    [moreinfo] Darken gradient on hover

--- src/components/home/moreinfo.js
+++ src/components/home/moreinfo.js
@@ -3,6 +3,7 @@ import { StaticQuery, graphql } from 'gatsby';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import { Container } from "react-bootstrap"
 import styled from "styled-components";
+import { FaAngleRight } from "react-icons/fa";
 
 const Section = styled.div`
   padding-top: 30px;
@@ -11,7 +12,7 @@ const Section = styled.div`
 `
 
 const Content = styled.div`
-  @media (min-width: 992px) {
+  @media (min-width: 1200px) {
     display: grid;
     grid-gap: 30px;
     grid-template-columns: repeat(3, 350px);
@@ -25,10 +26,12 @@ const Item = styled.div`
   & .gatsby-image-wrapper {
     transition: 0.5s;
   }
-  & :hover .gatsby-image-wrapper {
-    transform: scale(1.05);
+  & :hover {
+    .gatsby-image-wrapper {
+      transform: scale(1.05);
+    }
   }
-  @media (max-width: 992px) {
+  @media (max-width: 1200px) {
     & :not(:last-child) {
       margin-bottom: 30px;
     }
@@ -41,12 +44,14 @@ const ItemContent = styled.div`
   padding: 24px;
   align-self: stretch;
   color: var(--light);
-  background: rgba(0, 0, 0, 0.6);
-  background: linear-gradient(45deg, black, transparent);
+  background: linear-gradient(90deg, black, rgba(0, 0, 0, .5) 350px, transparent);
+  transition: 0.5s;
+  & :hover {
+    background: linear-gradient(90deg, black, rgba(0, 0, 0, .5));
+  }
   & h2 { 
     color: var(--light);
     font-size: 3.5rem;
-    margin-bottom: 1.5rem;
     align-self: stretch;
   }
   & h3 {
@@ -54,10 +59,28 @@ const ItemContent = styled.div`
     font-size: 2.4rem;
     font-weight: normal;
   }
+  & a {
+    color: var(--light);
+    padding-left: 0.37rem;
+    & svg {
+      color: var(--red);
+      transition: 0.5s;
+    }
+    & :hover, & :focus {
+      text-decoration: none;
+      svg {
+        transform: translateX(3px);
+      }
+    }
+  }
   display: flex;
   flex-direction: column;
   justify-content: flex-end;
   align-items: flex-start;
+`
+
+const TextBox = styled.div`
+  width: 250px;
 `
 
 function MoreInfo(props) {
@@ -70,11 +93,13 @@ function MoreInfo(props) {
         <GatsbyImage image={getImage(props.image)} alt=""
                      layout="fullWidth" style={{gridArea: "1/1"}} />
 	<ItemContent>
-	  <h3>{props.subtitle}</h3>
-          <h2>{props.title}</h2>
-	  <a href={props.link} className='btn btn-lg btn-primary stretched-link'>
-	    See More
-	  </a>
+	  <TextBox>
+	    <h3>{props.subtitle}</h3>
+            <h2>{props.title}</h2>
+	    <a href={props.link} className='stretched-link'>
+	      See More <FaAngleRight />
+	    </a>
+	  </TextBox>
 	</ItemContent>
       </Item>
     )
@@ -95,6 +120,7 @@ class MoreInfoComponent extends React.Component {
                 title
                 subtitle
                 description
+                link
                 image {
                   childImageSharp {
                     gatsbyImageData
@@ -116,7 +142,8 @@ class MoreInfoComponent extends React.Component {
                                    title={data.node.title}
                                    subtitle={data.node.subtitle}
                                    description={data.node.description}
-                                   image={data.node.image} />
+                                   image={data.node.image} 
+                                   link={data.node.link} />
                 })
               }
             </Content>
