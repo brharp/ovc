4bc4d5dd7ebd7b859de98c10f572da30e32e07f7
M. Brent Harp <brharp@uoguelph.ca>
2021-04-20 10:18:05 -0400

    Add call to action

---
+++ src/components/shared/calltoaction.js
@@ -0,0 +1,54 @@
+import React from "react"
+import { GatsbyImage } from "gatsby-plugin-image"
+import styled from "styled-components"
+import { Container } from "react-bootstrap"
+
+const Section = styled.div`
+  display: grid;
+  margin-bottom: 3rem;
+  & > * {
+    position: relative;
+    grid-area: 1/1;
+    display: grid;
+  }
+`
+
+const Content = styled.div`
+  & .btn {
+    font-size: 3rem;
+    padding: 1.5rem 2rem;
+  }
+`
+
+const Layout = styled.div`
+  grid-area: 1/1;
+  display: grid;
+  justify-items: center;
+  align-items: center;
+`
+
+class CallToAction extends React.Component {
+  render () {
+    return (
+      <Container>
+        <Section>
+          <GatsbyImage image={this.props.image}
+                     style={{gridArea: "1/1", maxHeight: "250px"}}
+                     layout="fullWidth"
+                     alt=""
+          />
+          <Layout>
+            <Content>
+              <a href={this.props.url} className="btn btn-primary">
+                {this.props.title}
+              </a>
+            </Content>
+          </Layout>
+        </Section>
+      </Container>
+    )
+  }
+}
+
+export default CallToAction
+
--- src/templates/portal.js
+++ src/templates/portal.js
@@ -4,11 +4,13 @@ import React from "react"
 import Layout from "../components/layout"
 import Banner from "../components/shared/banner"
 import Layers from "../components/shared/layers"
+import CallToAction from "../components/shared/calltoaction"
 
 class PortalPage extends React.Component {
   render() {
     const data = this.props.data
     const portal = data.portalsYaml
+    const cta = portal.cta
     return (
       <Layout>
         <Banner image={getImage(portal.image)}>
@@ -16,6 +18,11 @@ class PortalPage extends React.Component {
           <p>{portal.summary}</p>
         </Banner>
         <Layers topics={portal.sections} />
+        <CallToAction
+          title={cta.title}
+          url={cta.url}
+          image={getImage(cta.image)}
+          />
       </Layout>
     )
   }
@@ -38,7 +45,7 @@ export const query = graphql`
         url
         image {
           childImageSharp {
-            gatsbyImageData
+            gatsbyImageData(layout: FULL_WIDTH)
           }
         }
       }
