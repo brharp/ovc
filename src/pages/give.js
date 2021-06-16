3b878a955dbadfbdaf02647152c940e76d6e5471
M. Brent Harp <brharp@uoguelph.ca>
2021-06-16 15:18:44 -0400

    Refactor give page

--- src/pages/give.js
+++ src/pages/give.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 import Layout from '../components/layout'
 
@@ -44,8 +45,7 @@ const OptionsForGiving = () => (
 )
 
 
-const GivePage = ( props ) => <Layout>
-
+const Banner = () => (
   <div className="cover" style={{height: "600px"}}>
     <StaticImage className="cover-img" src="../images/university-centre.jpg" alt="" layout="fullWidth" />
     <div className="cover-img-overlay py-4 m-0 bg-black-50 h-100">
@@ -66,72 +66,83 @@ const GivePage = ( props ) => <Layout>
       </div>
     </div>
   </div>
+)
 
-  {/* content */}
-  <div className="container">
 
-    {/* Priorities */}
-    <div className="row py-4">
-      <div className="col-md">
-        <StaticImage src="../images/people.jpg" alt="" />
-      </div>
-      <div className="col-md order-md-first">
-        <h2>Advancing the OVC's Mission</h2>
-        <h3 className="text-dark">Your support makes a difference</h3>
-        <p class="lead">
-          Donations help the Ontario Veterinary College advance its
-          mission to educate veterinarians and scientists, create new
-          knowledge and provide expert services to improve the 
-          health and well-being of animals, people and the environment.
-        </p>
-        <p>
-          <a href="https://ovc.uoguelph.ca/" className="btn btn-primary btn-lg">
-            Explore OVC's Priorities
+
+const Priorities = () => (
+  <div className="row py-4">
+    <div className="col-md">
+      <StaticImage src="../images/people.jpg" alt="" />
+    </div>
+    <div className="col-md order-md-first">
+      <h2>Advancing the OVC's Mission</h2>
+      <h3 className="text-dark">Your support makes a difference</h3>
+      <p class="lead">
+        Donations help the Ontario Veterinary College advance its
+        mission to educate veterinarians and scientists, create new
+        knowledge and provide expert services to improve the 
+        health and well-being of animals, people and the environment.
+      </p>
+      <p>
+        <a href="https://ovc.uoguelph.ca/" className="btn btn-primary btn-lg">
+          Explore OVC's Priorities
+        </a>
+      </p>
+    </div>
+  </div>
+)
+
+
+const DonateNow = () => (
+  <div className="row">
+    <div className="col">
+      <div className="bg-blue-50 p-4">
+        <h2 className="text-center text-dark my-4">
+          Make a donation to support OVC today
+        </h2>
+        <p className="text-center">
+          <a href="https://ovc.uoguelph.ca" className="btn btn-lg btn-primary">
+            Donate Now
           </a>
         </p>
       </div>
     </div>
+  </div>
+)
 
 
-    {/* Donate Now */}
-    <div className="row">
-      <div className="col">
-        <div className="bg-blue-50 p-4">
-          <h2 className="text-center text-dark my-4">
-            Make a donation to support OVC today
-          </h2>
-          <p className="text-center">
-            <a href="https://ovc.uoguelph.ca" className="btn btn-lg btn-primary">
-              Donate Now
-            </a>
-          </p>
-        </div>
-      </div>
-    </div>
-
-
-
-    <OptionsForGiving />
-    <HowToGive />
-
-
-    <div className="card border-0 overflow-hidden mb-4">
-      <StaticImage src="../images/contact.png" alt=""
-                   layout="fullWidth"
-                   style={{ maxHeight: "200px" }}
-                   className="card-img" />
-      <div className="card-img-overlay">
-        <div className="container h-100">
-          <div className="row h-100 justify-content-center align-content-center">
-            <div className="btn btn-primary btn-cta">
-              Contact Us
-            </div>
-          </div>
+const Contact = () => (
+  <div className="card border-0 overflow-hidden mb-4">
+    <StaticImage src="../images/contact.png" alt=""
+                 layout="fullWidth"
+                 style={{ maxHeight: "200px" }}
+                 className="card-img" />
+    <div className="card-img-overlay">
+      <div className="container h-100">
+        <div className="row h-100 justify-content-center align-content-center">
+          <Link to="/contact-0" className="btn btn-primary btn-cta">
+            Contact Us
+          </Link>
         </div>
       </div>
     </div>
   </div>
-</Layout>
+)
+
+
+const GivePage = ( props ) => (
+  <Layout>
+    <Banner />
+    <div className="container">
+      <Priorities />
+      <DonateNow />
+      <OptionsForGiving />
+      <HowToGive />
+      <Contact />
+    </div>
+  </Layout>
+)
 
 export default GivePage
 
