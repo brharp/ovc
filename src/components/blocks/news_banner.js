5449e4fc9acfc7905da4036422bf00e7be5d7ac8
M. Brent Harp <brharp@uoguelph.ca>
2021-06-28 08:22:45 -0400

    Add news page

---
+++ src/components/blocks/news_banner.js
@@ -0,0 +1,38 @@
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import Banner from "../shared/banner"
+
+const render = ({ id, image, title, body }) => (
+  <Banner id={id}>
+    <GatsbyImage image={getImage(image)} className="cover-img" alt="" style={{height: "300px"}} />
+    <Banner.Overlay>
+      <Banner.Title>
+        {title}
+      </Banner.Title>
+      <Banner.Text>
+        {body}
+      </Banner.Text>
+    </Banner.Overlay>
+  </Banner>
+)
+
+const query = graphql`
+  query {
+    blockYaml(id: {eq: "news_banner"}) {
+      id
+      title
+      body
+      image {
+        childImageSharp {
+          gatsbyImageData
+        }
+      }
+    }
+  }
+`
+
+export default function NewsBanner () {
+  return <StaticQuery query={query} render={({blockYaml}) => render(blockYaml)} />
+}
+
---
+++ src/pages/news.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { Link, StaticQuery, graphql } from "gatsby"
+import { StaticImage, GatsbyImage, getImage } from "gatsby-plugin-image"
+import { Container, Row, Col } from "react-bootstrap"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import NewsBanner from "../components/blocks/news_banner"
+
+const render_row = ({title, body, image}) => (
+  <Row className="my-4">
+    <Col md={4}>
+      { image ?
+        <GatsbyImage image={getImage(image)} alt="" /> :
+        <StaticImage src="../components/news/default.jpg"  alt="" /> }
+    </Col>
+    <Col>
+      <h3>{title}</h3>
+      <p>{body}</p>
+      <Link to="https://ovc.uoguelph.ca/" className="btn btn-primary">
+        Read more<span className="sr-only"> about {title}</span>
+      </Link>
+    </Col>
+  </Row>
+)
+
+const render = (data) => (
+  <Layout>
+    <SEO title="News" />
+    <NewsBanner />
+    <Container className="my-4">
+      { data.map((i) => render_row(i)) }
+    </Container>
+  </Layout>
+)
+
+export const query = graphql`
+  query {
+    allNodeArticle {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          title
+          body {
+            summary
+          }
+          relationships {
+            field_hero_image {
+              relationships {
+                field_media_image {
+                  localFile {
+                    childImageSharp {
+                      gatsbyImageData(
+                        transformOptions: {cropFocus: ENTROPY},
+                      )
+                    }
+                  }
+                }
+              }
+            }
+            field_tags {
+              id
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+function makeArticles({edges}) {
+  return edges.map(({node}) => ({
+    title: node.title,
+    body: node.body.summary,
+    image: node.relationships.field_hero_image?.relationships.field_media_image.localFile,
+  }))
+}
+
+export default function NewsPage () {
+  return <StaticQuery query={query} render={({allNodeArticle}) => render(makeArticles(allNodeArticle))} />
+}
+
