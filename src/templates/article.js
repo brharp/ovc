7ef1d22d8c44aaa1e992d8734afa8d0a2ac34e37
M. Brent Harp <brharp@uoguelph.ca>
2021-06-28 16:30:38 -0400

    Add article template

--- src/templates/article.js
+++ src/templates/article.js
@@ -1,10 +1,65 @@
 import React from "react"
+import { graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { Container } from "react-bootstrap"
+import Banner from "../components/shared/banner"
 
-export default function ArticlePage() {
-    return (
-	    <Layout>
-	    <div>Article content...</div>
-	    </Layout>
-    )
+const ArticlePage = ({title, body, image}) => (
+  <Layout>
+    <SEO title={title} />
+    <Banner>
+      <GatsbyImage image={getImage(image)} alt="" className="cover-img"
+                   style={{height: "400px"}} />
+      <Banner.Overlay>
+        <h1 className="text-warning">{title}</h1>
+      </Banner.Overlay>
+    </Banner>
+    <Container className="py-4">
+      <div dangerouslySetInnerHTML={{__html: body}} />
+    </Container>
+  </Layout>
+)
+
+export const query = graphql`
+  query($slug: String) {
+    nodeArticle(fields: {slug: {eq: $slug}}) {
+      fields {
+        slug
+      }
+      title
+      body {
+        processed
+      }
+      relationships {
+        field_hero_image {
+          relationships {
+            field_media_image {
+              localFile {
+                childImageSharp {
+                  gatsbyImageData(
+                    transformOptions: {cropFocus: ENTROPY},
+                    layout: FULL_WIDTH,
+                  )
+                }
+              }
+            }
+          }
+        }
+        field_tags {
+          id
+          name
+        }
+      }
+    }
+  }
+`
+
+export default function DrupalArticlePage ({data}) {
+  return <ArticlePage title={data.nodeArticle.title}
+               body={data.nodeArticle.body.processed}
+               image={data.nodeArticle.relationships.field_hero_image?.relationships.field_media_image.localFile}
+               />
 }
+
