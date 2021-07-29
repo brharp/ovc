038b7533fa930f3b164359f161ae68718e4e7873
M. Brent Harp <brharp@uoguelph.ca>
2021-07-29 14:21:24 -0400

    Convert news spotlight to single article

--- src/components/blocks/news_more.js
+++ src/components/blocks/news_more.js
@@ -1,40 +1,50 @@
 import React from "react"
 import { Link, StaticQuery, graphql } from "gatsby"
 import { StaticImage, GatsbyImage, getImage } from "gatsby-plugin-image"
-import Banner from "../shared/banner"
-
-const render_item = ({title, body, image, slug}) => (
-  <Banner>
-    { image ?
-        <GatsbyImage image={getImage(image)} className="cover-img" style={{height: "600px"}} alt="" /> :
-        <StaticImage src="../news/default.jpg"  className="cover-img" style={{height: "600px"}} alt="" /> }
-    <Banner.Overlay className="px-4">
-      <Banner.Title>
-        {title}
-      </Banner.Title>
-      <Banner.Text>
-        <p>{body}</p>
-        <Link to={slug} className="btn btn-lg btn-primary">
-          Read more<span className="sr-only"> about {title}</span>
-        </Link>
-      </Banner.Text>
-    </Banner.Overlay>
-  </Banner>
-)
+import { Row, Col } from "react-bootstrap"
 
 const render = (articles) => (
-  articles.map((a) => render_item(a))
+  <div className="news-more">
+    <h1 className="text-dark">Articles of Interest</h1>
+    <hr className="w-25 ml-0" />
+    {
+      articles.map(({title, body, image, slug, changed}) => (
+        <Row className="my-4">
+          <Col md={4}>
+            {
+              image ? <GatsbyImage image={getImage(image)} alt="" />
+                    : <StaticImage src="../components/news/default.jpg"  alt="" />
+            }
+          </Col>
+          <Col>
+            <h3 className="text-dark mb-3">{title}</h3>
+            <p className="text-muted">{changed}</p>
+            <p>{body}</p>
+            <Link to={slug} className="btn btn-primary">
+              Read more<span className="sr-only"> about {title}</span>
+            </Link>
+          </Col>
+        </Row>
+      ))
+    }
+    <p className="text-center py-4">
+      <Link to="/newsfeed" className="btn btn-outline-primary btn-lg">
+        View Newsfeed
+      </Link>
+    </p>
+  </div>
 )
 
 const query = graphql`
   query {
-    allNodeArticle(limit: 1, filter: {promote: {eq: true}}) {
+    allNodeArticle(skip: 1, limit: 9, filter: {promote: {eq: true}}) {
       edges {
         node {
           fields {
             slug
           }
           title
+          changed(formatString: "MMMM DD, YYYY")
           body {
             summary
           }
@@ -46,6 +56,7 @@ const query = graphql`
                     childImageSharp {
                       gatsbyImageData(
                         transformOptions: {cropFocus: ENTROPY},
+                        aspectRatio: 1.33333333333333,
                       )
                     }
                   }
@@ -69,10 +80,11 @@ function makeArticles({edges}) {
     body: node.body.summary,
     image: node.relationships.field_hero_image?.relationships.field_media_image.localFile,
     slug: node.fields.slug,
+    changed: node.changed,
   }))
 }
 
-export default function NewsSpotlight () {
+export default function MoreNewsBlock () {
   return <StaticQuery query={query} render={({allNodeArticle}) => render(makeArticles(allNodeArticle))} />
 }
 
--- src/pages/news.js
+++ src/pages/news.js
@@ -1,47 +1,19 @@
 import React from "react";
-import { Link, StaticQuery, graphql } from "gatsby"
-import { StaticImage, GatsbyImage, getImage } from "gatsby-plugin-image"
 import { Container, Row, Col } from "react-bootstrap"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import NewsBanner from "../components/blocks/news_banner"
 import NewsSpotlight from "../components/blocks/news_spotlight"
+import NewsMore from "../components/blocks/news_more"
 import NewsTwitter from "../components/blocks/news_twitter"
 
-const render_row = ({title, body, image, changed, slug}) => (
-  <Row className="my-4">
-    <Col md={4}>
-      { image ?
-        <GatsbyImage image={getImage(image)} alt="" /> :
-        <StaticImage src="../components/news/default.jpg"  alt="" /> }
-    </Col>
-    <Col>
-      <h3 className="text-dark mb-3">{title}</h3>
-      <p className="text-muted">{changed}</p>
-      <p>{body}</p>
-      <Link to={slug} className="btn btn-primary">
-        Read more<span className="sr-only"> about {title}</span>
-      </Link>
-    </Col>
-  </Row>
-)
-
-const render = (data) => (
+export default function NewsPage(data) { return (
   <Layout>
     <SEO title="News" />
-    <NewsBanner />
     <Container className="my-4">
       <NewsSpotlight />
       <Row className="py-4">
         <Col lg={7}>
-          <h1 className="text-dark">Articles of Interest</h1>
-          <hr className="w-25 ml-0" />
-          { data.map((i) => render_row(i)) }
-          <p className="text-center py-4">
-            <Link to="/newsfeed" className="btn btn-outline-primary btn-lg">
-              View Newsfeed
-            </Link>
-          </p>
+          <NewsMore />
         </Col>
         <Col>
           <h1 className="text-dark">Social Feeds</h1>
@@ -51,58 +23,5 @@ const render = (data) => (
       </Row>
     </Container>
   </Layout>
-)
-
-export const query = graphql`
-  query {
-    allNodeArticle(limit: 10, filter: {sticky: {eq: false}, promote: {eq: true}}) {
-      edges {
-        node {
-          fields {
-            slug
-          }
-          title
-          body {
-            summary
-          }
-          changed(formatString: "MMMM DD, YYYY")
-          relationships {
-            field_hero_image {
-              relationships {
-                field_media_image {
-                  localFile {
-                    childImageSharp {
-                      gatsbyImageData(
-                        aspectRatio: 1.33333333333333333,
-                        transformOptions: {cropFocus: ENTROPY},
-                      )
-                    }
-                  }
-                }
-              }
-            }
-            field_tags {
-              id
-              name
-            }
-          }
-        }
-      }
-    }
-  }
-`
-
-function makeArticles({edges}) {
-  return edges.map(({node}) => ({
-    title: node.title,
-    body: node.body.summary,
-    image: node.relationships.field_hero_image?.relationships.field_media_image.localFile,
-    changed: node.changed,
-    slug: node.fields.slug,
-  }))
-}
-
-export default function NewsPage () {
-  return <StaticQuery query={query} render={({allNodeArticle}) => render(makeArticles(allNodeArticle))} />
-}
+)}
 
