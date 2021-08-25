cd56754b04d2f12400194afc26111fc06e46db7f
M. Brent Harp <brharp@uoguelph.ca>
2021-08-25 10:51:44 -0400

    Remove inline image parsing from body field

--- src/components/field/body.js
+++ src/components/field/body.js
@@ -1,42 +1,24 @@
 import React from "react"
-import { graphql, useStaticQuery } from "gatsby"
-import { GatsbyImage, getImage } from "gatsby-plugin-image"
-import HTMLReactParser from "html-react-parser"
 
-const Body = (props) => {
-  const data = useStaticQuery(graphql`
-    query { 
-      allFileFile(filter: { filemime: {regex: "/image\/(jpeg|png)/"} } ) {
-        edges {
-          node {
-            drupal_id
-            localFile {
-              childImageSharp {
-                gatsbyImageData(layout: CONSTRAINED)
-              }
-            }
-          }
-        }
-      }
+class Body extends React.Component {
+
+  static SUMMARY = 'summary';
+
+  render () {
+    switch (this.props.format) {
+      case Body.SUMMARY: return this.renderSummary(); break;
+      default: return this.renderProcessed(); break;
     }
-  `)
-  return HTMLReactParser(
-    props.format === "summary" ? props.summary : props.processed, {
-    replace: domNode => {
-      if (domNode.name === 'img') {
-        const uuid = domNode.attribs["data-entity-uuid"]
-        const files = data.allFileFile
-        const length = files.edges.length
-        for (let i = 0; i < length; i++) {
-          const file = files.edges[i].node
-          if (file.drupal_id === uuid && file.localFile) {
-            return <GatsbyImage image={getImage(file.localFile)} alt="" />
-          }
-        }
-      }
-      return undefined
-    }
-  });
+  }
+
+  renderSummary() {
+    return this.props.summary
+  }
+
+  renderProcessed() {
+    return this.props.processed
+  }
+
 }
 
 export default Body
