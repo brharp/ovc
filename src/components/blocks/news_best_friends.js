93a82c787cb0baaf2a34325ecafce0f398559300
M. Brent Harp <brharp@uoguelph.ca>
2021-09-03 10:17:08 -0400

    Add publication blocks to news page

---
+++ src/components/blocks/news_best_friends.js
@@ -0,0 +1,48 @@
+import React from "react"
+import { Link, StaticQuery, graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import Banner from "../shared/banner"
+
+const render = ({ id, image, title, body, link }) => (
+  <Banner>
+    <GatsbyImage image={getImage(image)} className="cover-img" alt=""
+                 style={{height: "500px"}}/>
+    <Banner.Overlay className="p-4 bg-black-80 ">
+      <div className="pl-3">
+        <h2 className="text-light">
+          {title}
+        </h2>
+        <p className="text-light">
+          {body}
+        </p>
+        <Link to={link.url} className="btn btn-primary btn-lg">
+          {link.title}
+        </Link>
+      </div>
+    </Banner.Overlay>
+  </Banner>
+)
+
+const query = graphql`
+  query {
+    blockYaml(id: {eq: "news_best_friends"}) {
+      id
+      title
+      body
+      link {
+        title
+        url
+      }
+      image {
+        childImageSharp {
+          gatsbyImageData(layout: CONSTRAINED)
+        }
+      }
+    }
+  }
+`
+
+export default function NewsBestFriends () {
+  return <StaticQuery query={query} render={({blockYaml}) => render(blockYaml)} />
+}
+
--- src/pages/news.js
+++ src/pages/news.js
@@ -5,6 +5,8 @@ import SEO from "../components/seo"
 import NewsSpotlight from "../components/blocks/news_spotlight"
 import NewsMore from "../components/blocks/news_more"
 import NewsTwitter from "../components/blocks/news_twitter"
+import NewsBestFriends from "../components/blocks/news_best_friends"
+import NewsCrest from "../components/blocks/news_crest"
 
 export default function NewsPage(data) { return (
   <Layout>
@@ -19,6 +21,14 @@ export default function NewsPage(data) { return (
           <h1 className="text-dark">Social Feeds</h1>
           <hr className="w-25 ml-0" />
           <NewsTwitter />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <NewsBestFriends />
+        </Col>
+        <Col>
+          <NewsCrest />
         </Col>
       </Row>
     </Container>
