9cee34162875600d89abaa61fcc53b66ea682d18
M. Brent Harp <brharp@uoguelph.ca>
2021-09-07 14:22:53 -0400

    Display inline images

--- src/components/field/body.js
+++ src/components/field/body.js
@@ -1,24 +1,41 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import HTMLReactParser from "html-react-parser"
 
-class Body extends React.Component {
+function Body(props) {
 
-  static SUMMARY = 'summary';
-
-  render () {
-    switch (this.props.format) {
-      case Body.SUMMARY: return this.renderSummary();
-      default: return this.renderProcessed();
+  const data = useStaticQuery(graphql`
+    query {
+      sitePlugin(name: {eq: "gatsby-source-drupal"}) {
+        pluginOptions {
+          baseUrl
+          apiBase
+        }
+      }
     }
+  `)
+
+  const renderSummary = () => {
+    return <div dangerouslySetInnerHTML={{__html: props.summary}}></div>
   }
 
-  renderSummary() {
-    return <div dangerouslySetInnerHTML={{__html: this.props.summary}}></div>
+  const renderProcessed = () => {
+    const baseUrl = data.sitePlugin.pluginOptions.baseUrl
+    return HTMLReactParser(props.processed, {
+      replace: domNode => {
+        if (domNode.name === 'img') {
+          const src = domNode.attribs['src'];
+          return <img src={`${baseUrl}.${src}`} alt="" />
+        }
+        return undefined
+      }
+    })
   }
 
-  renderProcessed() {
-    return <div dangerouslySetInnerHTML={{__html: this.props.processed}}></div>
+  switch (props.format) {
+    case 'summary': return renderSummary()
+    default: return renderProcessed()
   }
-
 }
 
 export default Body
