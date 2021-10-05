2cf524a67474afd19494d7c245b68aacf211457c
M. Brent Harp <brharp@uoguelph.ca>
2021-10-05 10:48:28 -0400

    Vertically center statistics

--- src/components/blocks/explore_statistics.js
+++ src/components/blocks/explore_statistics.js
@@ -27,40 +27,44 @@ export default function ExploreStatistics( props ) {
     <div className="cover">
       <StaticImage src="../../images/porticobannerfullsize.jpg" alt="" className="cover-img" />
       <div className="cover-img-overlay bg-black-50 p-4 text-center">
-        <Row>
-          <Statistic>
-            <Figure>482</Figure>
-            <Label>DVM Students</Label>
-          </Statistic>
-          <Statistic>
-            <Figure>127</Figure>
-            <Label>Faculty & Veterinarians</Label>
-          </Statistic>
-          <Statistic>
-            <Figure>1,336</Figure>
-            <Label>Graduate & B.Sc. Students</Label>
-          </Statistic>
-        </Row>
-          <Statistic>
-            <Figure size="lg">#1 in Canada</Figure>
-            <Label size="lg">#3 in North America</Label>
-            <Label size="lg">#5 in the world</Label>
-          </Statistic>
-        <Row>
-        </Row>
-        <Row>
-          <Statistic>
-            <Figure>20,829</Figure>
-            <Label>Pet & Equine Patient Visits</Label>
-          </Statistic>
-          <Statistic>
-            <Figure>$43,672,071</Figure>
-            <Label>Active Research Project Funding</Label>
-          </Statistic>
-          <Statistic>
-            <Figure>6,522</Figure>
-            <Label>Food Animal & Field Service Patient Visits</Label>
-          </Statistic>
+        <Row className="h-100">
+          <Col className="my-auto">
+            <Row className="my-auto">
+              <Statistic>
+                <Figure>482</Figure>
+                <Label>DVM Students</Label>
+              </Statistic>
+              <Statistic>
+                <Figure>127</Figure>
+                <Label>Faculty & Veterinarians</Label>
+              </Statistic>
+              <Statistic>
+                <Figure>1,336</Figure>
+                <Label>Graduate & B.Sc. Students</Label>
+              </Statistic>
+            </Row>
+            <Row>
+              <Statistic>
+                <Figure size="lg">#1 in Canada</Figure>
+                <Label size="lg">#3 in North America</Label>
+                <Label size="lg">#5 in the world</Label>
+              </Statistic>
+            </Row>
+            <Row>
+              <Statistic>
+                <Figure>20,829</Figure>
+                <Label>Pet & Equine Patient Visits</Label>
+              </Statistic>
+              <Statistic>
+                <Figure>$43,672,071</Figure>
+                <Label>Active Research Project Funding</Label>
+              </Statistic>
+              <Statistic>
+                <Figure>6,522</Figure>
+                <Label>Food Animal & Field Service Patient Visits</Label>
+              </Statistic>
+            </Row>
+          </Col>
         </Row>
       </div>
     </div>
