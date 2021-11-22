65d8fc35ee72b4d7638f282c7fea08b1b18f1207
M. Brent Harp <brharp@uoguelph.ca>
2021-11-22 16:31:14 -0500

    Update html.js

--- src/html.js
+++ src/html.js
@@ -10,6 +10,7 @@ export default function HTML(props) {
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="google-site-verification" content="QI8nLI8p_iG-r7plnNA2su0WwSMgDQpNrizQaIkEUEk" />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
